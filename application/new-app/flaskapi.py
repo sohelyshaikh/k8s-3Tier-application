82d5bc8f737889bd7dcffe22f710dd1aee9c01e4
sohelyshaikh <shaikh.sohely211@gmail.com>
2022-08-01 13:57:15 -0400

    building frontend application

--- application/new-app/flaskapi.py
+++ application/new-app/flaskapi.py
@@ -19,7 +19,7 @@ mysql.init_app(app)
 @app.route("/")
 def index():
     """Function to test the functionality of the API"""
-    return "Hello, world!"
+    return render_template(welcome.html)
 
 
 @app.route("/create", methods=["POST"])
